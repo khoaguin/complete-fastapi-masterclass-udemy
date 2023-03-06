ac650a0579af63fdfe3b557548369af75cf3de48
khoaguin <dkn.work@protonmail.com>
2023-03-06 15:05:50 +0700

    done summary and description

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Complete FastAPI Masterclass - Udemy
+
+My repor for the course [Complete FastAPI Masterclass from Scratch - Udemy](https://www.udemy.com/course/completefastapi/)
+
+## How to run
+
+`uvicorn app.<name_of_python_file>:app --reload`
---
+++ app/tags.py
@@ -0,0 +1,59 @@
+from enum import Enum
+from typing import Optional
+
+from fastapi import FastAPI, Response, status
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Homepage"}
+
+
+@app.get("/hello")
+def index():
+    return {"message": "Hello World"}
+
+
+@app.get(
+    path="/blog/all",
+    tags=["blogs"],
+)
+def get_all_blogs(page=1, page_size: Optional[int] = None):
+    return {"message": f"All {page_size} blogs on page {page}"}
+
+
+@app.get(
+    path="/blog/{id}/comments/{comment_id}",
+    tags=["blogs", "comments"],
+)
+def get_comment(
+    id: int, comment_id: int, valid: bool = True, username: Optional[str] = None
+):
+    return {
+        "message": f"blod with id {id}, comment id = {comment_id}, valid: {valid}, username is {username}"
+    }
+
+
+class BlogType(str, Enum):
+    short = "short"
+    story = "story"
+    howto = "howto"
+
+
+@app.get(
+    path="/blog/type/{type}",
+    tags=["blogs"],
+)
+def get_blog_type(type: BlogType):
+    return {"message": f"Blog type is {type}"}
+
+
+@app.get(path="/blog/{id}", tags=["blogs"], status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": f"blog {id} not found"}
+    else:
+        return {"message": f"Blog with id {id}"}
