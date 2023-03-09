57a47b312bcf4c8be8ba5c5be97fbacc5f31e61e
khoaguin <dkn.work@protonmail.com>
2023-03-09 10:48:51 +0700

    done updating and deleting users

--- db/db_user.py
+++ db/db_user.py
@@ -15,3 +15,31 @@ def create_user(db: Session, requests: UserBase):
     db.commit()
     db.refresh(new_user)  # to get the id (automatically generated) for the new user
     return new_user
+
+
+def get_all_users(db: Session):
+    return db.query(DBUser).all()
+
+
+def get_user(db: Session, id: int):
+    return db.query(DBUser).filter(DBUser.id == id).first()
+
+
+def update_user(db: Session, id: int, request: UserBase):
+    user = db.query(DBUser).filter(DBUser.id == id)
+    user.update(
+        {
+            DBUser.username: request.username,
+            DBUser.email: request.email,
+            DBUser.password: Hash.bcrypt(request.password),
+        }
+    )
+    db.commit()
+    return "user updated"
+
+
+def delete_user(db: Session, id: int):
+    user = db.query(DBUser).filter(DBUser.id == id).first()
+    db.delete(user)
+    db.commit()
+    return "user deleted"
--- router/user.py
+++ router/user.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -9,13 +11,30 @@ router = APIRouter(prefix="/user", tags=["user"])
 
 
 # create user
-@router.post("/", response_model=UserDisplay)
+@router.post(path="/", response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
 
 
-# read user
+# read all users
+@router.get(path="/users", response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+
+# read one user
+@router.get(path="/{id}", response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
+
 
 # update user
+@router.post(path="{id}/update")
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+    return db_user.update_user(db, id, request)
+
 
 # delete user
+@router.get(path="{id}/delete")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    return db_user.delete_user(db, id)
