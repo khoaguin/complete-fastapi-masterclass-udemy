7464659145f7c250eac10fadbfabaa9f4923fbb1
khoaguin <dkn.work@protonmail.com>
2023-03-14 22:36:16 +0700

    on learning section 8

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ My repor for the course [Complete FastAPI Masterclass from Scratch - Udemy](http
 
 ## Branch `database`
 
-Contain code for section 7 and 8 of the course
+Contain code for section 7 and 8 of the course. More specifically:
+
+- Section 7: The files in `db` that contain code for connecting to `SQLite` database using `SQLAlchemy`
+- Section 8: Concepts such as error handling, custom responses, headers, cookies... are in `exceptions.py` and `router/product.py`
 
 ## How to run
 
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from fastapi.responses import JSONResponse, PlainTextResponse
 from db import models
 from db.database import engine
 from exceptions import StoryException
-from router import article, blog_get, blog_posts, user
+from router import article, blog_get, blog_posts, product, user
 
 app = FastAPI()
 app.include_router(blog_get.router)
 app.include_router(blog_posts.router)
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(product.router)
 
 
 @app.get("/")
